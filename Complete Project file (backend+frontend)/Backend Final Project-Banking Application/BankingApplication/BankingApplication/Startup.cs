f819cf1f8780304b0c1b1e3187ff4c1c2b35d432
nikhil.satilal <nikhil.satilal@tssconsultancy.com>
2023-01-16 16:12:34 +0530

    Removed Unwanted imports

--- Complete Project file (backend+frontend)/Backend Final Project-Banking Application/BankingApplication/BankingApplication/Startup.cs
+++ Complete Project file (backend+frontend)/Backend Final Project-Banking Application/BankingApplication/BankingApplication/Startup.cs
@@ -4,17 +4,13 @@ using BankingApplication.IdProvider;
 using BankingApplication.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BankingApplication
 {
